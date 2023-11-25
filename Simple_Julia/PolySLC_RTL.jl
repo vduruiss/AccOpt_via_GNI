ea20a8ff49d4124fd2a3845a3d20ee8835f2415a
Valentin Duruisseaux <81578633+vduruiss@users.noreply.github.com>
2023-11-25 13:13:10 -0500

    Update PolySLC_RTL.jl

--- Simple_Julia/PolySLC_RTL.jl
+++ Simple_Julia/PolySLC_RTL.jl
@@ -1,7 +1,8 @@
 #= 
 This is a simple implementation of the PolySLC_RTL algorithm from
- "Practical Perspectives on Symplectic Accelerated Optimization"
- Authors: Valentin Duruisseaux and Melvin Leok. 2022.
+     "Practical Perspectives on Symplectic Accelerated Optimization"
+     Optimization Methods and Software, Vol.38, Issue 6, pages 1230-1268, 2023.
+     Authors: Valentin Duruisseaux and Melvin Leok. 
 
  Inputs:
 
