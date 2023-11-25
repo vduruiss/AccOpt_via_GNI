3225093d0353d44cfca279925c716db07565906d
Valentin Duruisseaux <81578633+vduruiss@users.noreply.github.com>
2023-11-25 13:15:44 -0500

    Update BrAVO_tf.py

--- TensorFlow_Codes/BrAVO_tf.py
+++ TensorFlow_Codes/BrAVO_tf.py
@@ -1,8 +1,9 @@
 # eBrAVO and pBrAVO Algorithms within TensorFlow
 '''
  Implementation of the eBrAVO and pBrAVO algorithms from
- "Practical Perspectives on Symplectic Accelerated Optimization"
- Authors: Valentin Duruisseaux and Melvin Leok. 2022.
+     "Practical Perspectives on Symplectic Accelerated Optimization"
+     Optimization Methods and Software, Vol.38, Issue 6, pages 1230-1268, 2023.
+     Authors: Valentin Duruisseaux and Melvin Leok. 
 '''
 
 
